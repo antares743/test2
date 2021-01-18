8c9a1618df511b17e564086f16c18b056e016f65
lucas <lucasx2002@hotmail.com>
2021-01-18 11:35:26 -0400

    fin

--- inc/dialog.h
+++ inc/dialog.h
@@ -12,6 +12,32 @@ class Dialog : public QDialog
  public:
  Dialog(QSqlRelationalTableModel *albums, QDomDocument details,
         QFile *output, QWidget *parent = nullptr);
+
+private slots:
+        void revert();
+        void submit();
+
+private:
+        int addNewAlbum(const QString &title,int artistId);
+        int addNewArtist(const QString &name);
+        void addTracks(int albumId, const QStringList &tracks);
+        QDialogButtonBox *createButtons();
+        QGroupBox *createInputWidgets();
+        int findArtistId(const QString &artist);
+        static int generateAlbumId();
+        static int generateArtistsId();
+        void increaseAlbumCount(QModelIndex artistIndex);
+        QModelIndex indexofArtist(const QString &artist);
+
+        QSqlRelationalTableModel *model;
+        QDomDocument albumDetails;
+        QFile *outputFile;
+
+        QLineEdit *artistEditor;
+        QLineEdit *titleEditor;
+        QSpinBox *yearEditor;
+        QLineEdit *tracksEditor;
 };
 
-#endif // DIALOG_H
+#endif
+
--- src/dialog.cpp
+++ src/dialog.cpp
@@ -78,3 +78,142 @@ int Dialog::addNewArtist(const QString &name)
     artistModel->insertRecord(-1,record);
     return id;
 }
+
+int Dialog::addNewAlbum(const QString &title, int artistId)
+{
+    int id = generateAlbumId();
+    QSqlRecord record;
+
+    QSqlField f1("albumid",QVariant::Int);
+    QSqlField f2("title", QVariant::String);
+    QSqlField f3("artistid",QVariant::Int);
+    QSqlField f4("years",QVariant::Int);
+
+    f1.setValue(QVariant(id));
+    f2.setValue(QVariant(title));
+    f3.setValue(QVariant(artistId));
+    f4.setValue(QVariant(yearEditor->value()));
+    record.append(f1);
+    record.append(f2);
+    record.append(f3);
+    record.append(f4);
+
+    model->insertRecord(-1,record);
+    return id;
+}
+void Dialog::addTracks(int albumId, const QStringList &tracks)
+{
+    QDomElement albumNode = albumDetails.createElement("album");
+    albumNode.setAttribute("id",albumId);
+
+    for(int i = 0; i < tracks.count(); ++i){
+        QString trackNumber = QString::number(i);
+        if (i<10)
+            trackNumber.prepend('0');
+
+        QDomText textNode = albumDetails.createTextNode(tracks.at(i));
+
+        QDomElement trackNode = albumDetails.createElement("tarck");
+        trackNode.setAttribute("number", trackNumber);
+        trackNode.appendChild(textNode);
+
+        albumNode.appendChild(trackNode);
+    }
+
+    QDomNodeList archive = albumDetails.elementsByTagName("archive");
+    archive.item(0).appendChild(albumNode);
+
+    }
+void Dialog::increaseAlbumCount(QModelIndex artistIndex)
+{
+    QSqlTableModel *artistModel = model->relationModel(2);
+
+    QModelIndex albumContIndex;
+    albumContIndex = artistIndex.sibling(artistIndex.row(),2);
+
+    int albumCount = albumContIndex.data().toInt();
+    artistModel->setData(albumContIndex,QVariant(albumCount + 1));
+}
+void Dialog::revert()
+{
+    artistEditor->clear();
+    titleEditor->clear();
+    yearEditor->setValue(QDate::currentDate().year());
+    tracksEditor->clear();
+}
+QGroupBox *Dialog::createInputWidgets()
+{
+    QGroupBox *box = new QGroupBox(tr("add album"));
+
+    QLabel *artistLabel = new QLabel(tr("artist:"));
+    QLabel *titleLabel = new QLabel(tr("title:"));
+    QLabel *yearLabel = new QLabel(tr("year:"));
+    QLabel *tracksLabel = new QLabel(tr("tracks (separated by coma):"));
+
+    artistEditor = new QLineEdit;
+    titleEditor = new QLineEdit;
+
+    yearEditor = new QSpinBox;
+    yearEditor->setMinimum(1990);
+    yearEditor->setMaximum(QDate::currentDate().year());
+    yearEditor->setValue(yearEditor->maximum());
+    yearEditor->setReadOnly(false);
+
+    tracksEditor = new QLineEdit;
+
+    QGridLayout *layout = new QGridLayout;
+    layout->addWidget(artistLabel,0,0);
+    layout->addWidget(artistEditor,0,1);
+    layout->addWidget(titleLabel,1,0);
+    layout->addWidget(titleEditor,1,1);
+    layout->addWidget(yearLabel,2,0);
+    layout->addWidget(yearEditor,2,1);
+    layout->addWidget(tracksLabel,3,0,1,2);
+    layout->addWidget(tracksEditor,4,0,1,2);
+    box->setLayout(layout);
+
+    return box;
+}
+QDialogButtonBox *Dialog::createButtons()
+{
+    QPushButton *closeButton = new QPushButton(tr("&Close"));
+    QPushButton *revertButton = new QPushButton(tr("&Revert"));
+    QPushButton *submitButton = new QPushButton(tr("&Submit"));
+
+    closeButton->setDefault(true);
+
+    connect(closeButton, &QPushButton::clicked,this,&Dialog::close);
+    connect(revertButton,&QPushButton::clicked,this, &Dialog::revert);
+    connect(submitButton, &QPushButton::clicked, this, &Dialog::submit);
+
+    QDialogButtonBox *buttonBox = new QDialogButtonBox;
+    buttonBox->addButton(submitButton, QDialogButtonBox::ResetRole);
+    buttonBox->addButton(revertButton, QDialogButtonBox::ResetRole);
+    buttonBox->addButton(closeButton, QDialogButtonBox::RejectRole);
+
+    return buttonBox;
+}
+QModelIndex Dialog::indexofArtist(const QString &artist)
+{
+    QSqlTableModel *artistModel = model->relationModel(2);
+
+    for (int i = 0; i < artistModel->rowCount(); ++i){
+        QSqlRecord record = artistModel->record(i);
+        if(record.value("artist") == artist)
+            return artistModel->index(i, 1);
+    }
+    return QModelIndex();
+}
+int Dialog::generateArtistsId()
+{
+    uniqueArtistId += 1;
+    return uniqueArtistId;
+}
+int Dialog::generateAlbumId()
+{
+    uniqueAlbumId += 1;
+    return uniqueAlbumId;
+}
+
+
+
